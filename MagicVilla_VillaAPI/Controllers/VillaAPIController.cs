3e38408b21a91279ecec4349fd2ae6c5f3929e0b
Brayan Sandí Barrantes <sandi.bryansteef22@gmail.com>
2025-07-02 20:10:46 -0600

    Creación de primer endpoint de prueba

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaModel> GetVillas()
+        {
+            return new List<VillaModel>
+            {
+                new VillaModel { Id = 1, Name = "Villa 1" },
+                new VillaModel { Id = 2, Name = "Villa 2" }
+            };
+        }
+    }
+}
