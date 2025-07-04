7414e17f5cc48315c4f5e057d7b306e00103b135
Brayan Sandí Barrantes <sandi.bryansteef22@gmail.com>
2025-07-04 17:02:31 -0600

    Actualización de modelos para crear base de datos

--- MagicVilla_VillaAPI/Models/VillaModel.cs
+++ MagicVilla_VillaAPI/Models/VillaModel.cs
@@ -1,11 +1,19 @@
-﻿namespace MagicVilla_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
 {
     public class VillaModel
     {
+        [Key]
         public int Id { get; set; }
-
         public string Name { get; set; }
-
-        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
     }
 }
